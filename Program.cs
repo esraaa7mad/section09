d3a4492ed6707d7ac7fda242379e8f8620aebe9c
Esraa Ahmad Ali Bedair <100854428+esraaa7mad@users.noreply.github.com>
2022-12-16 20:10:33 +0200

    task7

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
 ﻿using System;
 public class Person{
- private string _Name;
+ private string _name;   
+ public string Name
+ {
+    get 
+    {
+        return _name;
+    } 
+    set
+    {
+         if(value == null || value=="" || value.Length>=32)
+    {
+        throw new Exception("invalid name");
+    } 
+        _name=value;
+    }
+ }
  private int _Age;
  public Person(string name,int age)
  {
@@ -12,31 +27,46 @@ public class Person{
     Console.WriteLine("invalid age");
     return;
   } 
-  _Name=name;
+  Name=name;
   _Age =age;
  }
- public string Getname( ) =>_Name;
+ 
  public int Getage() =>_Age;
- public void Setname(string name)
- {
-    if(name == null || name=="" || name.Length>=32)
-  {
-    throw new Exception("invalid name");
-  } 
-    _Name=name;
- }
+ 
  
  public virtual  void Print()
     {
-      Console.WriteLine($"My name is {_Name}, my age is {_Age}");
+      Console.WriteLine($"My name is {Name}, my age is {_Age}");
     }
 
 }
 
 public class Student :  Person
 {
-    public int Year;
-    public float Gpa;
+    private int _Year;
+    public int Year
+    {
+      get 
+    {
+        return _Year;
+    } 
+    set
+    {
+        _Year=value;
+    }   
+    }
+    private float _Gpa;
+    public float Gpa
+    {
+       get 
+    {
+        return _Gpa;
+    } 
+    set
+    {
+        _Gpa=value;
+    }      
+    }
     public Student(string name,int age,int year,float gpa ):base(name, age)
     {
         if (year>1 ||year<5)
@@ -52,13 +82,35 @@ public class Student :  Person
     }
     public override void Print()
     {
-        Console.WriteLine($"my name is{Getname()},my age is {Getage()},and my gpa is{Gpa}");
+        Console.WriteLine($"my name is{Name},my age is {Getage()},and my gpa is{Gpa}");
     }
 }
 public class Stuff :Person 
 {
-    public int JoinYear;
-    public double Salary;
+    private int _JoinYear;
+    public int JoinYear
+    {
+          get 
+    {
+        return _JoinYear;
+    } 
+    set
+    {
+        _JoinYear=value;
+    }   
+    }
+    private double _Salary;
+    public double Salary
+    {
+          get 
+    {
+        return _Salary;
+    } 
+    set
+    {
+        _Salary=value;
+    }   
+    }
     public Stuff(string name,int age,int joinyear,double salary ):base(name, age)
     
     {
@@ -75,7 +127,7 @@ public class Stuff :Person
     }
      public override void Print()
     {
-        Console.WriteLine($"my name is{Getname()},my age is {Getage()},and my salary is{Salary}");
+        Console.WriteLine($"my name is{Name},my age is {Getage()},and my salary is{Salary}");
     }
 }
 public class Database
